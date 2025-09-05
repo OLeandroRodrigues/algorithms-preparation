a34b648552879fc92ff7e47d7588e161510a6e1d
Leandro Rodrigues <91847576+OLeandroRodrigues@users.noreply.github.com>
2025-09-04 23:48:30 -0300

    feat: Write inserting sort algorithm

---
+++ python/algorithms/sorting/insertion_sort/insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    
+    for i in range(1,len(arr)):
+        
+        key = arr[i] # current element
+        j = i - 1
+
+        # move the elements biggest than key from position in the front
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        
+        arr[j + 1] = key
+
+    return arr
