a62f64b3ccd163491629932b3fbb027b06b494b7
Leandro Rodrigues <91847576+OLeandroRodrigues@users.noreply.github.com>
2025-10-23 11:01:06 -0300

    feat: Add quicksort algorithm implemented in python

---
+++ python/algorithms/sorting/quick_sort/quick_sort.py
@@ -0,0 +1,27 @@
+def quick_sort(arr, low, high):
+    if low < high:
+        # index of the pivot after partition
+        p = partition(arr, low, high)
+
+        quick_sort(arr, low, p - 1)  # left side
+        quick_sort(arr, p + 1, high) # righ side
+    
+
+def partition(arr,low,high):
+    pivot = arr[high] 
+    i = low - 1 # smallest element index
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+# Sample of use
+if __name__ == "__main__":
+    A = [2, 8, 7, 1, 3, 5, 6]
+    print("After:", A)
+    quick_sort(A, 0, len(A) - 1)
+    print("Before", A)
